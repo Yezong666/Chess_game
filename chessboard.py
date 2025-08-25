91667fe57ad818538d59ee9215b7cb71be888981
Yezong <yemisa666@gmail.com>
2025-08-25 20:12:45 +0200

    highlight some tiles

--- chessboard.py
+++ chessboard.py
@@ -2,6 +2,7 @@ import pygame
 
 COLOR_RED = (84, 0, 0)
 COLOR_WHITE = (255, 255, 255)
+DARK_YELLOW = (186,142,35)
 WIDTH_SIDE = 100
 
 
@@ -19,26 +20,26 @@ class Chess_board():
             ["wR", "wK", "wB", "wQu", "wKi", "wB", "wK", "wR"]
         ]
 
-    #draw the chessboard
+    #draw the chessboard(initialisation)
     def draw(self, screen):
         flipcolor = 0
         x = 0
         y = 0
         for line in self.chessboard:
-            for piece in line:
+            for tile in line:
                 if flipcolor % 2:
                     pygame.draw.rect(screen, COLOR_RED, (x*WIDTH_SIDE, y *WIDTH_SIDE,WIDTH_SIDE ,WIDTH_SIDE))   
                 else:
                     pygame.draw.rect(screen, COLOR_WHITE, (x*WIDTH_SIDE, y *WIDTH_SIDE,WIDTH_SIDE ,WIDTH_SIDE))
-                if piece != "":
-                    self.put_piece(screen, piece, x, y)
+                if tile != "":
+                    self.put_piece(screen, tile, x, y)
                 flipcolor+=1
                 x+=1
             x = 0
             y += 1
             flipcolor+=1
                 
-        
+     
     def put_piece(self, screen, piece, x, y):
         match piece:
             case "wP":
@@ -69,19 +70,82 @@ class Chess_board():
                 print("ERROR SUPPOSED TO NEVER HAPPEN"
                       f"piece was {piece}")
                 
-            
-        
-        
-        
-        
         screen.blit(img, (x*WIDTH_SIDE, y*WIDTH_SIDE))
 
 
 
+    #Highlight the piece selected, and return its location
+    def select_piece(self, screen, chessboard, x, y, selected_piece):
+        if selected_piece[0] != False:
+            if (selected_piece[1] + selected_piece[2]) %2:
+                pygame.draw.rect(screen, COLOR_RED, (selected_piece[1]*WIDTH_SIDE, selected_piece[2] *WIDTH_SIDE,WIDTH_SIDE ,WIDTH_SIDE))
+            else: 
+                pygame.draw.rect(screen, COLOR_WHITE, (selected_piece[1]*WIDTH_SIDE, selected_piece[2] *WIDTH_SIDE,WIDTH_SIDE ,WIDTH_SIDE))
+            self.put_piece(screen, chessboard[selected_piece[2]][selected_piece[1]], selected_piece[1] ,selected_piece[2])
 
-    def highlight(self, chessboard, x, y):
-        pass
-        #Highlight the moves of the piece in coordinates x, y : do it later
+        if chessboard[y][x] != "":
+            pygame.draw.rect(screen, DARK_YELLOW, (x*WIDTH_SIDE, y *WIDTH_SIDE,WIDTH_SIDE ,WIDTH_SIDE))
+            self.put_piece(screen, chessboard[y][x], x ,y)
+            return (chessboard[y][x], x, y)
+        return (False, 0, 0)
+    
+
+    #Return a list of possible move and highlight the tiles the piece can move to
+    def check_moves(self, screen, chessboard, selected_piece):
+        color_piece = selected_piece[0][0]
+        piece = selected_piece[0][1:]
+        moves = []
+        match piece:
+            case "P":
+                if color_piece == "w":
+                    
+                    if selected_piece[2] == 6:
+                        num_moves = 2
+                    else:
+                        num_moves = 1
+                    i = 1
+                    while (chessboard[selected_piece[2]-i][selected_piece[1]]) == "" and num_moves > 0:
+                        moves.append((selected_piece[1],selected_piece[2]-i))
+                        pygame.draw.rect(screen, DARK_YELLOW, (selected_piece[1]*WIDTH_SIDE, (selected_piece[2]-i) *WIDTH_SIDE,WIDTH_SIDE ,WIDTH_SIDE))
+                        num_moves -= 1
+                        i+=1            
+                else:
+                    if selected_piece[2] == 1:
+                        num_moves = 2
+                    else:
+                        num_moves = 1
+                    i = 1
+                    while (chessboard[selected_piece[2]+i][selected_piece[1]]) == "" and num_moves > 0:
+                        moves.append((selected_piece[1],selected_piece[2]+i))
+                        pygame.draw.rect(screen, DARK_YELLOW, (selected_piece[1]*WIDTH_SIDE, (selected_piece[2]+i) *WIDTH_SIDE,WIDTH_SIDE ,WIDTH_SIDE))
+                        num_moves -= 1
+                        i+=1
+            case "R":
+                pass
+            case "K":
+                pass
+            case "B":
+                pass
+            case "Qu":
+                pass
+            case "Ki":
+                pass
+
+            case _:
+                print("ERROR SUPPOSED TO NEVER HAPPEN"
+                      f"piece was {piece}")
+                
+        return moves
+    
+
+    def remove_highlighted_moves(self, screen, chessboard, highlighted_moves):
+        for move in highlighted_moves:
+            if (move[0] + move[1]) %2:
+                pygame.draw.rect(screen, COLOR_RED, (move[0]*WIDTH_SIDE, move[1] *WIDTH_SIDE,WIDTH_SIDE ,WIDTH_SIDE))
+            else: 
+                pygame.draw.rect(screen, COLOR_WHITE, (move[0]*WIDTH_SIDE, move[1] *WIDTH_SIDE,WIDTH_SIDE ,WIDTH_SIDE))
+            if chessboard[move[1]][move[0]] != "":
+                self.put_piece(screen, chessboard[move[1]][move[0]], move[0] ,move[1])
     
 
 
--- main.py
+++ main.py
@@ -16,16 +16,25 @@ def main():
 
     chess_board = Chess_board()
     chess_board.draw(screen)
+    selected_piece = [False, 0, 0]      #(Piece, x, y)
+    possible_moves = []
     dt = 0
     while(1):
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 0
-            
-        #pygame.Surface.fill(screen, (0,0,0))
-           
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_loc = pygame.mouse.get_pos()
+                mouse_loc_x = (mouse_loc[0] - (mouse_loc[0] % 100)) / 100
+                mouse_loc_y = (mouse_loc[1]  - (mouse_loc[1] % 100)) / 100
+                selected_piece = chess_board.select_piece(screen, chess_board.chessboard, int(mouse_loc_x), int(mouse_loc_y), selected_piece)
+                chess_board.remove_highlighted_moves(screen, chess_board.chessboard, possible_moves)
+                if selected_piece[0] != False:
+                    possible_moves = chess_board.check_moves(screen, chess_board.chessboard, selected_piece)
         
+           
+        print(possible_moves)
         pygame.display.flip()
         
         
